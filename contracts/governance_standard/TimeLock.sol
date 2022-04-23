044c89f71bb9c559a90af8c4692ea45ab2549dc2
Victor Inojosa <vijoin@gmail.com>
2022-04-23 17:21:12 -0300

    Add TimelockController contract

---
+++ contracts/governance_standard/TimeLock.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/governance/TimelockController.sol";
+
+contract TimeLock is TimelockController {
+    constructor(
+        uint256 minDelay, // How long you have to wait before executing
+        address[] memory proposers, // list of addresses that can propose
+        address[] memory executors // who can execute when a a proposal passes
+    ) TimelockController(minDelay, proposers, executors) {}
+}
