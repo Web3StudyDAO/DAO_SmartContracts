af0fcec9fa72c871d4191a1815d1eb312bab7012
Victor Inojosa <vijoin@gmail.com>
2022-04-23 21:59:59 -0300

    Deploy subscription token and transfer ownership and all supply

--- contracts/W3DAOToken.sol
+++ contracts/W3DAOToken.sol
@@ -2,9 +2,19 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Web3DAOToken is ERC20 {
+contract Web3DAOToken is ERC20, Ownable {
     constructor(uint initialSupply) ERC20("Web3DAO", "W3DAO") {
         _mint(msg.sender, initialSupply);
     }
+
+    function mint(uint256 amount) public onlyOwner {
+        _mint(msg.sender, amount);
+    }
 }
+
+// TODO: Make it ownable
+// TODO: create mint function allowing only to the owner
+// TODO: transfer ownership in the deploy script
+// TODO: create unit tests for mint function
---
+++ deploy/05-deploy-subscription-token.ts
@@ -0,0 +1,32 @@
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import {DeployFunction} from 'hardhat-deploy/types';
+import { ethers } from 'hardhat';
+import { time } from 'console';
+
+const deploySubscriptionToken: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+    const { getNamedAccounts, deployments} = hre;
+    const { deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts();
+    log("Deploying Subscription Token Contract...");
+
+    const initialSupply = await ethers.utils.parseEther("1000000");
+    const token = await deploy("Web3DAOToken", {
+        from: deployer,
+        args: [initialSupply],
+        log: true,
+    });
+
+    const timeLock = await ethers.getContract("TimeLock");
+    const tokenContract = await ethers.getContractAt("Web3DAOToken", token.address);
+    const transferOwnershipTx = await tokenContract.transferOwnership(timeLock.address);
+    await transferOwnershipTx.wait(1);
+    const transferTokensToOwnerTx = await tokenContract.transfer(timeLock.address, initialSupply);
+    await transferTokensToOwnerTx.wait(1);
+
+    log(`Deployed Web3DAOToken Contract to address ${token.address}`)
+
+
+    // transfer everything to the timelock
+};
+
+export default deploySubscriptionToken;
