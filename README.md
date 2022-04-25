2416335b8585f547f1bf64c292bb805a12317738
Victor Inojosa <vijoin@gmail.com>
2022-04-25 09:53:44 -0300

    Deploy everything to Polygon Mumbai
    * Governance token at 0x61689cc7777D3bE8dEE5e3d4d7633b7721714648
    * TimeLock at 0xa53ba6016c8D6d02Ee8a4417093F25Ce3C89cD59
    * Governor contract at: 0x5C5608860aCF0A2A7A082CD8c764Ad972220Ba90
    * Web3DAOToken Contract at 0xf7a060590ac2Df0719a62F5CeC19105FAC2a8517

--- README.md
+++ README.md
@@ -13,3 +13,18 @@ npx hardhat node
 node scripts/sample-script.js
 npx hardhat help
 ```
+# Live Network Deploy commmands used
+
+```
+ npx hardhat --network matic deploy --tags "governance_token"
+ npx hardhat --network matic deploy --tags "timelock"
+ npx hardhat --network matic deploy --tags "governor"
+ npx hardhat --network matic deploy --tags "setup"
+ npx hardhat --network matic deploy --tags "token"
+```
+
+# Polygon Mumbai contracts
+* Governance token at 0x61689cc7777D3bE8dEE5e3d4d7633b7721714648
+* TimeLock at 0xa53ba6016c8D6d02Ee8a4417093F25Ce3C89cD59
+* Governor contract at: 0x5C5608860aCF0A2A7A082CD8c764Ad972220Ba90
+* Web3DAOToken Contract at 0xf7a060590ac2Df0719a62F5CeC19105FAC2a8517
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,28 +2,9 @@ import "@typechain/hardhat";
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-ethers";
 import "hardhat-deploy";
-
 import { HardhatUserConfig } from "hardhat/types";
-
-// // This is a sample Hardhat task. To learn how to create your own go to
-// // https://hardhat.org/guides/create-task.html
-// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-//   const accounts = await hre.ethers.getSigners();
-
-//   for (const account of accounts) {
-//     console.log(account.address);
-//   }
-// });
-
-// // You need to export an object to set up your config
-// // Go to https://hardhat.org/config/ to learn more
-
-// /**
-//  * @type import('hardhat/config').HardhatUserConfig
-//  */
-// module.exports = {
-//   solidity: "0.8.9",
-// };
+import { PRIVATE_KEY } from "./env";
+import { ethers } from "hardhat";
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
@@ -34,6 +15,12 @@ const config: HardhatUserConfig = {
     },
     localhost: {
       chainId: 31337,
+    },
+    matic: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [PRIVATE_KEY],
+      gasPrice: 8e9,
+      // gas: 4e17
     }
   },
   namedAccounts: {
